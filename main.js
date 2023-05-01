e2675a3c7b7d1e3abcf2689fba18af3ed27b2a58
tanya <ttnlzvsk@mail.ru>
2023-05-01 18:02:46 +0300

    feat: add en and ru keyboards

--- main.js
+++ main.js
@@ -23,6 +23,17 @@ keyboard.classList.add('main-container__keyboard');
 operationSystem.classList.add('main-container__system');
 language.classList.add('main-container__language');
 
+textArea.setAttribute("readonly", true);
+let usedEng = true;
+
+const userLanguage = document.documentElement.lang;
+if (userLanguage == "en") {
+    usedEng = true;
+}
+else {
+    usedEng = false;
+}
+console.log(usedEng);
 
 const codeArr1 = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'];
 const symbolArr1E = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
@@ -43,48 +54,92 @@ const symbolArr4E = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 
 const codeArr5 = ['CtrlLeft', 'OptLeft', 'CmdLeft', 'Space', 'CmdRight', 'OptRight', 'CtrlRight'];
 const symbolArr5E = ['Ctrl', 'Opt', 'Cmd', ' ', 'Cmd', 'Opt', 'Ctrl'];
 
-const symbolArr1 = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
-const symbolArr2 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'];
-const symbolArr3 = ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'];
-const symbolArr4 = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift'];
+const capsSymbolsE1 = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const capsSymbolsE2 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'];
+const capsSymbolsE3 = ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'];
+const capsSymbolsE4 = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift'];
 
 
 const symbolArr1Ru = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-const ArrOfCapsKeysRus1 = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const capsSymbolsRu1 = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
 
 const symbolArr2Ru = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'];
-const ArrOfCapsKeysRus2 = ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', '{', '}', '|'];
+const capsSymbolsRu2 = ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', '{', '}', '|'];
 
 const symbolArr3Ru = ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'];
-const ArrOfCapsKeysRus3 = ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'];
+const capsSymbolsRu3 = ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'];
 
 const symbolArr4Ru = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift'];
-const ArrOfCapsKeysRus4 = ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '?', 'Shift'];
+const capsSymbolsRu4 = ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '?', 'Shift'];
 
 
 function createButton(elementCodeClass, elementKey) {
-  const buttonElement = document.createElement("button");
-  const buttonSymbol = document.createTextNode(`${elementKey}`);
+    const buttonElement = document.createElement("button");
+    const buttonSymbol = document.createTextNode(`${elementKey}`);
 
-  keyboard.appendChild(buttonElement);
-  buttonElement.appendChild(buttonSymbol);
-  buttonElement.classList.add(elementCodeClass);
-  buttonElement.classList.add("key");
+    keyboard.appendChild(buttonElement);
+    buttonElement.appendChild(buttonSymbol);
+    buttonElement.classList.add(elementCodeClass);
+    buttonElement.classList.add("key");
 
 }
 
 function createKeyboard(codeArr, symbolArr) {
     for (let i = 0; i < codeArr.length; i++) {
         createButton(codeArr[i], symbolArr[i]);
-      }
+    }
 }
 
 function createEngKeyboard() {
+    deleteKeyboard()
     createKeyboard(codeArr1, symbolArr1E);
     createKeyboard(codeArr2, symbolArr2E);
     createKeyboard(codeArr3, symbolArr3E);
     createKeyboard(codeArr4, symbolArr4E);
     createKeyboard(codeArr5, symbolArr5E);
-  }
+    usedEng = true;
+}
 
-createEngKeyboard();
+
+function createRuKeyboard() {
+    deleteKeyboard()
+    createKeyboard(codeArr1, symbolArr1Ru);
+    createKeyboard(codeArr2, symbolArr2Ru);
+    createKeyboard(codeArr3, symbolArr3Ru);
+    createKeyboard(codeArr4, symbolArr4Ru);
+    createKeyboard(codeArr5, symbolArr5E);
+    usedEng = false;
+}
+
+function createEngKeyboardUsingCaps() {
+    deleteKeyboard()
+    createKeyboard(codeArr1, capsSymbolsE1);
+    createKeyboard(codeArr2, capsSymbolsE2);
+    createKeyboard(codeArr3, capsSymbolsE3);
+    createKeyboard(codeArr4, capsSymbolsE4);
+    createKeyboard(codeArr5, symbolArr5E);
+    usedEng = true;
+}
+
+
+function createRuKeyboardUsingCaps() {
+    deleteKeyboard()
+    createKeyboard(codeArr1, capsSymbolsRu1);
+    createKeyboard(codeArr2, capsSymbolsRu2);
+    createKeyboard(codeArr3, capsSymbolsRu3);
+    createKeyboard(codeArr4, capsSymbolsRu4);
+    createKeyboard(codeArr5, symbolArr5E);
+    usedEng = false;
+}
+
+
+function deleteKeyboard() {
+    keyboard.innerHTML = '';
+}
+
+if (usedEng) {
+    createEngKeyboard();
+} else {
+    createRuKeyboard();
+}
+
